8482b1fa28620fb6253f69b48ab0c91630be876e
dcarsin <dcarsin@byui.edu>
2021-10-02 21:11:33 -0300

    David Carsin

--- package_week3/__main__.py
+++ package_week3/__main__.py
@@ -3,10 +3,12 @@ import meisman
 import pregnolato
 import claure
 import lewis
+import carsin
 print("Welcome to our collaborative program.")
 
 claure.greetings_cla()
 pregnolato.greetings_pregnolato()
 meisman.greeting_meisman()
 lewis.greeting_lewis()
+carsin.gretting_carsin()
 
---
+++ package_week3/carsin.py
@@ -0,0 +1,2 @@
+def gretting_carsin():
+    print("Cheers to all from Uruguay!")
